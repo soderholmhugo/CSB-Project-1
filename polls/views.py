415a8072ef0cebfecc18e1c6ed14c72bd1e9e7f7
Dravde01 <hugosoderholm@hotmail.com>
2023-12-27 17:49:51 +0200

    revert generics, add login/logout/addquestion

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,54 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
-from django.views import generic
+from django import forms
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import logout
 
 from .models import Choice, Question
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+def logout(request):
+    logout(request)
+    return redirect('login')
 
-    def get_queryset(self):
-        # Return the last five published questions
-        return Question.objects.order_by('pub_date')[:5]
+def login(request):
+    return render(request, 'polls/login.html')
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+class AddQuestionForm(forms.Form):
+    question = forms.CharField(max_length=200)
+    choice1 = forms.CharField(max_length=200)
+    choice2 = forms.CharField(max_length=200)
+
+@csrf_exempt # 1. Remove decorator, uncomment csrf-middleware in settings.py and uncomment csrf-token in index.html
+def addquestion(request):
+    #if request.method == 'POST':
+        #form = AddQuestionForm(request.POST)
+        #if form.is_valid():
+    question = request.GET.get("q")
+    choice1 = request.GET.get("c1")
+    choice2 = request.GET.get("c2")
+
+    q = Question(question_text=question, pub_date=datetime.now())
+    q.save()
+    q.choice_set.create(choice_text=choice1, votes=0)
+    q.choice_set.create(choice_text=choice2, votes=0)
+    q.save()
+    form = AddQuestionForm()
+    return HttpResponseRedirect(reverse('index'))
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -37,4 +66,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
